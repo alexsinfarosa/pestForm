caf7ba66e7f845fc7e378dcad25ca8aa73415103
alexsinfarosa <as898@cornell.edu>
2017-07-26 15:45:43 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
-import { PestTable2 } from "components/PestTable2";
-import { observer, inject } from "mobx-react";
+import PestTable from "components/PestTable";
 
-@inject("store")
-@observer
 class App extends Component {
   render() {
     return (
       <div>
-        <PestTable2 />
+        <PestTable />
       </div>
     );
   }
